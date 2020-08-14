9b63560a10b8c295770ddd4ed4dac08a90d016a1
leandromac <contato@leandromacedo.com>
2020-08-14 10:27:35 -0300

    Trabalhando com formatação de datas

--- js/controller/controller.js
+++ js/controller/controller.js
@@ -1,24 +1,60 @@
 class CalcController {
 
   constructor() {
-    this._displayCalc = "0";
-    this._dataAtual;
+    this._locale = 'pt-BR';
+    this._displayCalcEl = document.querySelector('#display');
+    this._dateEl = document.querySelector('#date');
+    this._timeEl = document.querySelector('#hour');
+    this._currentDate;
+    this.initialize();
+  }
+
+  initialize() {
+    this.setDisplayDateTime();
+    setInterval(()=>{
+      this.setDisplayDateTime();
+    }, 1000);
+  } 
+
+  setDisplayDateTime() {
+    this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
+      day: "2-digit",
+      month: "long",
+      year: "numeric"
+    });
+    this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+  }
+
+  get displayTime() {
+    return this._timeEl.innerHTML;
+  }
+
+  set displayTime(value) {
+    return this._timeEl.innerHTML = value;
+  }
+
+  get displayDate() {
+    return this._dateEl.innerHTML;
+  }
+
+  set displayDate(value) {
+    return this._dateEl.innerHTML = value;
   }
 
   get displayCalc() {
-    return this._displayCalc;
+    return this._displayCalcEl.innerHTML;
   }
 
-  set displayCalc(valor) {
-    this.displayCalc = valor;
+  set displayCalc(value) {
+    this.displayCalcEl.innerHTML = value;
   }
 
-  get dataAtual() {
-    return this._dataAtual;
+  get currentDate() {
+    return new Date();
   }
 
-  set dataAtual(valor){
-    this._dataAtual = valor;
+  set currentDate(value) {
+    this._currentDate = value;
   }
 
 }
